63bfc0706da8b73556acae0b0dfec07dbeae47f5
mohitk74 <163093956+mohitk74@users.noreply.github.com>
2024-06-20 15:55:58 +0530

    Update automating_test_manager.m

--- automating_test_manager.m
+++ automating_test_manager.m
@@ -1,4 +1,11 @@
 open('switch_test_case.mldatx');
 sltest.testmanager.run()
 sltest.testmanager.view;
-% kkk
+sltest.testmanager.load('switch_test_case.mldatx');
+%--------------------------------------------------------------------------
+% Executing the test manager
+%-------------------------------------------------------------------------
+% Report Generation
+sltest.testmanager.report(result,'test_repot.pdf',...
+    'IncludeTestResults',0,'IncludeComparisonSignalPlots',true,...
+    'IncludeSimulationSignalPlots',true,'NumPlotRowsPerPage',3);
