734500d600bd5d6a4b7856b5821a67da97a676fb
39216 <39216@39216-445824.TELXSIDS.NET>
2024-06-24 17:13:40 +0530

    h

--- automating_test_manager.m
+++ automating_test_manager.m
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 
-open('and_testfile.mldatx');
-runtests('and_testfile.mldatx')
-% sltest.testmanager.run()
-sltest.testmanager.view;
-% k
-=======
 open('switch_test_case.mldatx');
 runtests('switch_test_case.mldatx')
 % sltest.testmanager.run()
 sltest.testmanager.view;
->>>>>>> cd26e0dac540b4570c691d08fe158d11e62a79ec
+% 
+
+% open('switch_test_case.mldatx');
+% runtests('switch_test_case.mldatx')
+% % sltest.testmanager.run()
+% sltest.testmanager.view;
+% 
